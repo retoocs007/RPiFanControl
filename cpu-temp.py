07118ed27ee414d877e5723cdaa0a6fded6f0bbf
retoocs007 <klabazan@gmail.com>
2017-08-15 15:47:51 +0200

    Temperature test script
    code cleenup

--- cpu-temp.py
+++ cpu-temp.py
@@ -5,12 +5,9 @@ import time
 cpu = CPUTemperature()
 
 while True:
-
     currdate = (time.strftime("%d/%m/%Y"))
     currtime = (time.strftime("%H:%M:%S"))
     currtemp = int(cpu.temperature)
-
     print '{} {} Current CPU temperature: {} '.format(currdate, currtime, currtemp)
-
     # Wait before the next iteration
     sleep(10)
